42f87e11e17d28818c1d05ddbfb8a9914d8548bf
ElbertRibeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-06-04 19:24:34 -0300

    criação firebase Config

---
+++ src/Components/firebaseConfig.js
@@ -0,0 +1,7 @@
+const config = {
+    apiKey: "AIzaSyCHS_BNjn1tOn5iMR8nDim5ezFPztL9RR8",
+    authDomain: "desafio-react-25354.firebaseapp.com",
+    databaseURL: "https://desafio-react-25354.firebaseio.com",
+  };
+  
+export default config;
