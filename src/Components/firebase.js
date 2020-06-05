4f787c959acd6eb79eb14ecb2a6d0d4202b7c4df
ElbertRibeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-06-05 19:44:57 -0300

    config de ambiente para integração com database

---
+++ src/Components/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAgnsKeaXAlMUYp7u_r5ZITbdsdSdy11Ho",
+    authDomain: "desafio-react-cca56.firebaseapp.com",
+    databaseURL: "https://desafio-react-cca56.firebaseio.com",
+    projectId: "desafio-react-cca56",
+    storageBucket: "desafio-react-cca56.appspot.com",
+    messagingSenderId: "845430550915",
+    appId: "1:845430550915:web:2823a63e2ac7b0a021c6b6"
+};
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
--- src/Components/firebaseConfig.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    apiKey: "AIzaSyCHS_BNjn1tOn5iMR8nDim5ezFPztL9RR8",
-    authDomain: "desafio-react-25354.firebaseapp.com",
-    databaseURL: "https://desafio-react-25354.firebaseio.com",
-  };
-  
-export default config;
