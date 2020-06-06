abbd49b501cef1f726943de46d8078c8a112b320
ElbertRibeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-06-06 17:01:31 -0300

    add table list

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
+import ServerTable from 'react-strap-table';
 
 import '../assets/App.css';
 
-// eslint-disable-next-line no-unused-vars
-import firebase from '../Components/firebase';
-
-let db = firebase.database();
-
-export default class App extends React.Component {
 
 
-  render() {
-    return(
-      <table>
-        {db.map(db => (
-          <tr key={db.id}>
-            <td>{db.option}</td>
-            <td>{db.type}</td>
-          </tr>
-      ))}
-      </table>
-    )
-  }
+export default  function App() {
+  const url = 'https://react-strap-table.com/users';
+  const columns = ['id', 'name', 'email', 'created_at'];
+  const options = {  
+     headings: {id: '#', created_at: 'Created At'},  
+     sortable: ['name', 'email']  
+  };  
+  return (  
+      <ServerTable columns={columns}  
+       url={url}  
+       options={options} 
+       bordered hover/>  
+  );
 
 }
