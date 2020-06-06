404ae4ba363dc75ac47b01da74c27ff53b76f24f
ElbertRibeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-06-06 16:29:32 -0300

    update App.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
 
 import '../assets/App.css';
-// eslint-disable-next-line
+
+// eslint-disable-next-line no-unused-vars
 import firebase from '../Components/firebase';
 
+let db = firebase.database();
+
+export default class App extends React.Component {
 
 
+  render() {
+    return(
+      <table>
+        {db.map(db => (
+          <tr key={db.id}>
+            <td>{db.option}</td>
+            <td>{db.type}</td>
+          </tr>
+      ))}
+      </table>
+    )
+  }
 
-export default function App() {
-
-  return (
-    <div>
-      teste
-    </div>
-  );
 }
-
